ee6e539f2caac05ee49a8bd4bfb54c8aa7fe639c
rabiiyaa01 <bibirabia2727@gmail.com>
2025-11-21 10:40:58 +0530

    Add files via upload

---
+++ attach1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+void names(char first[],char last[],int sw)
+{
+    int i=0;
+    char full[100];
+    while(first[i]!='\0') //adding first
+    {
+        full[i]=first[i];
+        i++;
+    }
+    full[i]=' '; //adding space
+    i++;
+    int j=0;
+    while(last[j]!= '\0') //adding last
+    {
+        full[i]=last[j];
+        i++;
+        j++;int len=i+1;
+    }
+    printf("%s" ,full);
+    int len=i+1;
+    if(len<sw)
+    printf(" fits in the screen");
+    else
+    printf(" does not fit in the screen");
+    
+    
+}
+
+void main()
+{
+    char first[100]="Bibi";
+    char last[100]="Rabiyaaaa";
+    int sw=20;
+    names(first,last,sw);
+
+}
