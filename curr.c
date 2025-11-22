f901d3d4783bdda2cda0d6e0dc4520cb92a10e20
rabiiyaa01 <bibirabia2727@gmail.com>
2025-11-22 10:40:00 +0530

    Add files via upload

---
+++ curr.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+void fake_swap(float ca,float cb)
+{
+    float temp=0;
+    temp=ca;
+    ca=cb;
+    cb=temp;
+    printf("fake swap in function: ca=%f cb=%f\n",ca,cb);
+}
+void real_swap(float *pca, float *pcb)
+{
+    float temp=0;
+    temp=*pca;
+    *pca=*pcb;
+    *pcb=temp;
+    printf("real swap in function: *ca=%f *cb=%f\n", *pca,*pcb);
+}
+void main()
+{
+    float ca,cb;
+    printf("enter currency A: ");
+    scanf("%f",&ca);
+    printf("enter currency B: ");
+    scanf("%f",&cb);
+
+    fake_swap(ca,cb);
+    printf("fake swap: ca= %f cb=%f\n",ca,cb);
+
+    real_swap(&ca,&cb); 
+    printf("real swap: ca= %f cb=%f\n",ca,cb);
+}
---
+++ struct_calls.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+struct score{
+    float marks;
+    int rank;
+};
+
+void edit(struct score s1)
+{
+    printf("before change in function: %f\n",s1.marks);
+    s1.marks=89.7;
+    printf("after change in function: %f\n",s1.marks);
+}
+void main()
+{
+    struct score s1={45.6,3};
+    edit(s1);
+    printf("after change in main %f\n",s1.marks);
+}
