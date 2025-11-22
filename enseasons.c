efa7662b9405adf367c8b4c470acdbb739607e35
rabiiyaa01 <bibirabia2727@gmail.com>
2025-11-22 14:57:09 +0530

    Add files via upload

---
+++ enseasons.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+enum months
+{
+    jan=1,
+    feb,march,april,may,june,july,august,sept,oct,nov,dec
+};
+void main()
+{
+    enum months m;
+    m=sept;
+    char *season;
+    switch(m)
+    {
+        case dec: case jan: case feb:
+        season= "winter";
+         break;
+        case march: case april: case may:
+        season="summer"; 
+        break;
+        case june: case july: case august:
+        season="monsoon"; 
+        break;
+        case sept: case oct: case nov:
+        season="spring"; 
+        break;
+    }
+    {
+    printf("%s ", season);
+    }
+
+
+}
